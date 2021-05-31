20d569e13a763208da3b477f07ae6a9b495b20a4
Gary Wolff <wolff@freedomfflow.com>
2021-05-31 12:44:54 -0400

    Initial commit

---
+++ dist/impulseven.js
@@ -0,0 +1,23 @@
+
+function changeText(id) {
+  let text = {
+    'segment1': { 'pct': "23.5%", 'label': 'farming' },
+    'segment2': { 'pct': "17%", 'label': 'Private Round' },
+    'segment3': { 'pct': "14%", 'label': 'Strategic Round' },
+    'segment4': { 'pct': "12%", 'label': 'Team/Advisor' },
+    'segment5': { 'pct': "9%", 'label': 'Development Fund' },
+    'segment6': { 'pct': "8.5%", 'label': 'Dex liquidity' },
+    'segment7': { 'pct': "8%", 'label': 'Marketing' },
+    'segment8': { 'pct': "5%", 'label': 'Reserve' },
+    'segment9': { 'pct': "3%", 'label': 'Public Sale' },
+  }
+
+  let element = document.getElementById('chart-text');
+  element.innerHTML = "<text x='50%' y='50%' class='chart-number' id='chart-text'>" + text[id].pct + "</text>" +
+    "<text x='50%' y='50%' class='chart-label' id='chart-text'>" + text[id].label + "</text>";
+}
+
+function restoreText() {
+  let element = document.getElementById('chart-text');
+  element.innerHTML = "<text x='50%' y='52%' class='chart-number' id='chart-text'>IV</text";
+}
